5f28a0bc419b36c3e4e64c677aea0fa388cf03d6
Nikhil Gupta <108614432+nikhilgame8@users.noreply.github.com>
2022-10-07 08:18:38 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,61 @@
-import React from 'react'
-import '../styles/App.css';
+import React, { useEffect, useState } from "react";
 
-const Loader = () => <div id="loader">Loading...</div>
+import "../styles/App.css";
 
+ 
+
+const Loader = () => <div id="loader">Loading...</div>;
+
+ 
 
 const App = () => {
 
-  
+ const [activity, setActivity] = useState("");
 
-  return (
-    <div id="main">
-    
-    </div>
-  )
-}
+ const [type, setType] = useState("education");
 
+ const [loading , setLoading] = useState(false)
+
+ 
+
+async function fetching() {
+
+ setLoading(true)
+
+ let res = await fetch(`https://www.boredapi.com/api/activity?type=${type}`);
+
+ let data = await res.json();
+
+ setLoading(false)
+
+ setActivity(data.activity)
+
+ }
+
+ useEffect(() => {
+
+ fetching();
+
+ }, [type]);
+
+ 
+
+return (
+
+ <div id="main">
+
+ {loading ? (<Loader/>) : (<div id="activity">{activity}</div>)}
+
+ <button id="btn-recreation" onClick={() => setType("recreational")}>Recreational</button>
+
+ <button id="btn-education" onClick={() => setType("education")}>Education</button>
+
+ </div>
+
+ );
+
+};
+
+ 
 
 export default App;
